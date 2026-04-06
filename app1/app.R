83e8176d7d6271fb6686377c98ce8f32e58abc4e
theCodedMike <lixiaofeng1115@outlook.com>
2026-04-07 02:30:33 +0800

    学习shiny

---
+++ app1/app.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(bslib)
+
+# Define UI ----
+ui = page_sidebar(
+    title = "My Shiny App",
+    sidebar = sidebar(
+        "Shiny is available on CRAN, so you can install it in the usual way from your R console:",
+        code('install.packages("shiny")')
+    ),
+    card(
+        card_header("Introducing Shiny"),
+        card_body(
+            "Shiny is a package from Posit that makes it incredibly easy to build interactive web applications with R. For an introduction and live examples, visit the Shiny homepage (https://shiny.posit.co).",
+            min_height = 100
+        ),
+        card_image("./dog_and_duck.jpg", height = 500),
+        card_footer("Shiny is a product of Posit.")
+    )
+)
+
+#ui = page_fluid(
+#    title = "Test App",
+#    layout_sidebar(
+#        sidebar = sidebar("侧边栏"),
+#        "Main Contents"
+#    )
+#)
+
+# Define server logic ----
+server = function(input, output) {}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
--- r_shiny.R
+++ r_shiny.R
@@ -8,22 +8,21 @@ any(grepl("bslib", installed.packages())) #TRUE
 # 载入包
 library(shiny)
 
-# 查看示例1
-runExample("01_hello")
+# 查看官方提供的示例1
+#runExample("01_hello", display.mode = "showcase")      # a histogram
+#runExample("02_text", display.mode = "showcase")       # tables and data frames
+#runExample("03_reactivity", display.mode = "showcase") # a reactive expression
+#runExample("04_mpg", display.mode = "showcase")        # global variables
+#runExample("05_sliders", display.mode = "showcase")    # slider bars
+#runExample("06_tabsets", display.mode = "showcase")    # tabbed panels
+#runExample("07_widgets", display.mode = "showcase")    # help text and submit buttons
+#runExample("08_html", display.mode = "showcase")       # Shiny app built from HTML
+#runExample("09_upload", display.mode = "showcase")     # file upload wizard
+#runExample("10_download", display.mode = "showcase")   # file download wizard
+#runExample("11_timer", display.mode = "showcase")      # an automated timer
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 运行本地web应用
+#运行方式1: 运行以下的代码
+#runApp("1_hello", display.mode = "normal") #"01_hello"是目录的名字
+#  display.mode: normal(默认值，页面不显示代码)，而showcase会显示代码
+#运行方式2: 打开app.R文件，然后Ctrl+Shift+Enter
